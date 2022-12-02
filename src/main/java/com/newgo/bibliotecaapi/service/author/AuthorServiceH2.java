d8e5e3e5ef882495da0f9e974929211460a203db
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-02 15:34:45 -0300

    add author service h2 implementation

---
+++ src/main/java/com/newgo/bibliotecaapi/service/author/AuthorServiceH2.java
@@ -0,0 +1,47 @@
+package com.newgo.bibliotecaapi.service.author;
+
+import com.newgo.bibliotecaapi.model.author.Author;
+import com.newgo.bibliotecaapi.repository.AuthorRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Profile("dev")
+@Service
+public class AuthorServiceH2 implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceH2(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    @Transactional
+    public Author save(Author author) {
+        return this.authorRepository.save(author);
+    }
+
+    @Override
+    public Optional<Author> findById(UUID id) {
+        return this.authorRepository.findById(id);
+    }
+
+    @Override
+    public Set<Author> findAll() {
+        Set<Author> authors = new HashSet<>();
+        this.authorRepository.findAll().forEach(authors::add);
+        return authors;
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(UUID id) {
+        this.authorRepository.deleteById(id);
+    }
+}
