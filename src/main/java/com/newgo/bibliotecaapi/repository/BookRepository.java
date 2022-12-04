29e89a93857b70c3e63937ae2152fb96b1211b88
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-04 16:14:23 -0300

    add book repository

---
+++ src/main/java/com/newgo/bibliotecaapi/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.newgo.bibliotecaapi.repository;
+
+import com.newgo.bibliotecaapi.dto.BookDTO;
+import com.newgo.bibliotecaapi.model.book.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, UUID> {
+}
