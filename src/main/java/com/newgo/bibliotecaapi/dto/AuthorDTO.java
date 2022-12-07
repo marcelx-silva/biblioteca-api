d836993f229e1b5c9cdc3b67366705e7f8940707
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-07 15:00:05 -0300

    add set of books to dto

--- src/main/java/com/newgo/bibliotecaapi/dto/AuthorDTO.java
+++ src/main/java/com/newgo/bibliotecaapi/dto/AuthorDTO.java
@@ -2,20 +2,24 @@ package com.newgo.bibliotecaapi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
+import java.util.Set;
 
-@Getter
 @Setter
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AuthorDTO {
-    @NotBlank
+    @NotBlank(message = "authors name should not be null")
     @Min(value = 10)
     private String name;
     @Past
     private LocalDate birthDate;
+    private Set<BookAuthorDTO> books;
 }
