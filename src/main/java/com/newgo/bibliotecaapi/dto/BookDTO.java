a018b54c0c7095876dbbbe1962d72710cd0b5ec5
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-07 14:57:08 -0300

    add another attributes to dto

--- src/main/java/com/newgo/bibliotecaapi/dto/BookDTO.java
+++ src/main/java/com/newgo/bibliotecaapi/dto/BookDTO.java
@@ -2,21 +2,33 @@ package com.newgo.bibliotecaapi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
-import java.time.LocalDate;
 import java.util.Set;
+import java.util.UUID;
 
-@Getter
 @Setter
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookDTO {
     @NotBlank
     @Min(value = 10)
     private String title;
+    @NotBlank
+    private String genre;
+    @NotBlank
+    private String publisher;
+    @NotBlank
+    private String language;
+    @NotBlank
+    private Integer pages;
+    @NotBlank
+    private String isbn13;
+    private Set<UUID> authors;
 
-    private Set<AuthorDTO> authorDTOSet;
+
 }
