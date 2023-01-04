1aca75ef9a91c48fa616fef4b0288bbb2d04179e
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2023-01-04 18:26:11 -0300

    add abstract class to audit entities

--- src/main/java/com/newgo/bibliotecaapi/model/BaseEntity.java
+++ src/main/java/com/newgo/bibliotecaapi/model/BaseEntity.java
@@ -1,23 +1,22 @@
 package com.newgo.bibliotecaapi.model.baseentity;
 
 
+import com.newgo.bibliotecaapi.model.AbstractAuditableEntity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
-@MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
-public class BaseEntity {
+@MappedSuperclass
+
+public abstract class BaseEntity extends AbstractAuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
